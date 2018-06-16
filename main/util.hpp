66de8d90c774b5247da2d6602892850f91ed4dfd
Stephen Oliver <steve@infincia.com>
2018-06-16 16:01:00 -0400

    [Util] add round() and map() functions

--- main/util.hpp
+++ main/util.hpp
@@ -6,6 +6,10 @@
 //unsigned long micros();
 unsigned long millis();
 
+double round(double d);
+
+long map(int input, int input_start, int input_end, int output_start, int output_end);
+
 bool volume_up();
 bool volume_down();
 bool volume_set(uint8_t level);
