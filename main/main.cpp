d6ea42979b5ef76e92a67f453184d2f2a3f54aa4
Stephen Oliver <steve@infincia.com>
2018-06-16 16:08:02 -0400

    [All] replace colons with dashes in device_id

--- main/main.cpp
+++ main/main.cpp
@@ -310,7 +310,7 @@ static void device_setup_task(void *param) {
     uint8_t mac[6];
     esp_wifi_get_mac(ESP_IF_WIFI_STA, mac);
     char ser[32] = {0,};
-    sprintf(ser, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    sprintf(ser, "%02X-%02X-%02X-%02X-%02X-%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
     std::string device_id(ser);
 
