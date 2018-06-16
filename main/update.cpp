8b4506d045f53be3eb3e0a52555485028264c098
Stephen Oliver <steve@infincia.com>
2018-06-16 19:16:24 -0400

    [OTA] separate updater from individual checks
    Prevents a memory leak caused by throwing exceptions before
    esp_ota_end can be run

--- main/update.cpp
+++ main/update.cpp
@@ -16,52 +16,15 @@ extern const char letsencrypt_chain_pem_end[] asm("_binary_letsencrypt_chain_pem
 
 
 /**
- * @brief Task wrapper
- */
-
-static void task_wrapper(void *param) {
-	auto *instance = static_cast<Update *>(param);
-    instance->task();
-}
-
-
-/**
  * @brief Update
  *
  */
 
-Update::Update(const char* update_manifest_url): update_url(update_manifest_url) { }
+Update::Update(const char* update_manifest_url, std::string& device_name): 
+_device_name(device_name), 
+_update_url(update_manifest_url) {
 
-
-Update::~Update() = default;
-
-
-void Update::start(std::string& device_name) {
-    this->device_name = device_name;
-
-    ESP_LOGI(TAG, "start");
-
-    xTaskCreate(&task_wrapper, "update_task", 8192, this, (tskIDLE_PRIORITY + 10),
-        &this->update_task_handle);
-
-}
-
-
-/**
- *
- * @brief Update handler
- */
-
-bool Update::update(const char* url) {
     esp_err_t err;
-    esp_ota_handle_t update_handle;
-
-    const esp_partition_t *configured = esp_ota_get_boot_partition();
-    const esp_partition_t *running = esp_ota_get_running_partition();
-    const esp_partition_t *update_partition = esp_ota_get_next_update_partition(running);
-
-    /* an image total length */
-    int binary_file_length = 0;
 
     if (configured != running) {
         ESP_LOGW(
@@ -71,25 +34,57 @@ bool Update::update(const char* url) {
         running->subtype, running->address);
 
     if (!update_partition) {
-        ESP_LOGE(TAG, "failed to get update partition");
-        return false;
+        throw std::runtime_error("failed to get update_partition");
     }
 
     ESP_LOGD(TAG, "next ota partition subtype %d at offset 0x%x", update_partition->subtype, update_partition->address);
 
 
-    err = esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &update_handle);
+    err = esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &_update_handle);
     if (err != ESP_OK) {
         const char* msg = error_string(err);
         ESP_LOGE(TAG, "esp_ota_begin failed: %s", msg);
-        return false;
+        throw std::runtime_error("esp_ota_begin failed");
     }
+ }
+
+
+Update::~Update() {
+    esp_err_t err;
+
+    err = esp_ota_end(_update_handle);
+    if (err != ESP_OK) {
+        const char* msg = error_string(err);
+        ESP_LOGE(TAG, "esp_ota_end failed: %s", msg);
+        return;
+    }
+
+    ESP_LOGI(TAG, "updating OTA boot partition");
+
+    err = esp_ota_set_boot_partition(update_partition);
+    if (err != ESP_OK) {
+        const char* msg = error_string(err);
+        ESP_LOGE(TAG, "boot select error: %s", msg);
+    }   
+};
+
+/**
+ *
+ * @brief Update handler
+ */
+
+void Update::check() {
+    esp_err_t err;
+
+    /* an image total length */
+    int binary_file_length = 0;
+
 
     long timeout = 5000;
 
 
     char user_agent[32];
-    snprintf(user_agent, sizeof(user_agent), "%s/%s", this->device_name.c_str(), FIRMWARE_VERSION);
+    snprintf(user_agent, sizeof(user_agent), "%s/%s", this->_device_name.c_str(), FIRMWARE_VERSION);
 
 
 
@@ -100,13 +95,13 @@ bool Update::update(const char* url) {
 
     int res;
     try {
-        memset(text, 0, TEXT_BUFFSIZE);
+        memset(this->_text, 0, TEXT_BUFFSIZE);
 
         http_client.set_read_cb([&] (const char* buf, int length) {
-            memcpy(text, buf, length);
+            memcpy(_text, buf, length);
         });
 
-        res = http_client.get(url);
+        res = http_client.get(_update_url);
 
         if (res >= 500) {
             throw std::runtime_error("server error");
@@ -121,7 +116,7 @@ bool Update::update(const char* url) {
         
         ESP_LOGD(TAG, "http request success: %d", res);
 
-        std::string body(text);
+        std::string body(_text);
 
         auto m = JSON::parseObject(body);
 
@@ -132,40 +127,41 @@ bool Update::update(const char* url) {
 
         if (available_version_s.length() == 0) {
             ESP_LOGE(TAG, "available_version key missing");
-            return false;
+            throw std::runtime_error("available_version key missing");
         }
         if (firmware_url.length() == 0) {
             ESP_LOGE(TAG, "firmware_url key missing");
-            return false;
+            throw std::runtime_error("firmware_url key missing");
         }
     } catch (std::exception &ex) {
         ESP_LOGE(TAG, "manifest download failed: %s", ex.what());
-        return false;
+        throw std::runtime_error("manifest download failed");
     }
 
 
     try {
-        Semver current_version(FIRMWARE_VERSION);
-        Semver available_version(available_version_s);
+        ESP_LOGI(TAG, "current git version: %s", FIRMWARE_VERSION);
 
+        Semver current_version(FIRMWARE_VERSION);
         ESP_LOGI(TAG, "current version: %s", current_version.string().c_str());
+
+        Semver available_version(available_version_s);
 
         ESP_LOGI(TAG, "found latest update: %s", available_version.string().c_str());
 
         if (available_version <= current_version) {
-            ESP_LOGI(TAG, "no update available");
-            return false;
+            throw std::runtime_error("no update available");
         }
     } catch (std::exception &ex) {
         ESP_LOGE(TAG, "version check failed: %s", ex.what());
-        return false;    
+        throw std::runtime_error("version check failed");
     }
 
     ESP_LOGI(TAG, "beginning firmware upgrade");
 
     try {
         http_client.set_read_cb([&] (const char* buf, int length) {
-            err = esp_ota_write(update_handle, buf, length);
+            err = esp_ota_write(_update_handle, buf, length);
             if (err != ESP_OK) {
                 const char* msg = error_string(err);
                 ESP_LOGD(TAG, "esp_ota_write: %s", msg);
@@ -195,52 +191,6 @@ bool Update::update(const char* url) {
     }
 
     ESP_LOGI(TAG, "finished writing firmware to flash, closing OTA session");
-
-    err = esp_ota_end(update_handle);
-    if (err != ESP_OK) {
-        const char* msg = error_string(err);
-        ESP_LOGE(TAG, "esp_ota_end failed: %s", msg);
-        return false;
-    }
-
-    ESP_LOGI(TAG, "updating OTA boot partition");
-
-    err = esp_ota_set_boot_partition(update_partition);
-    if (err != ESP_OK) {
-        const char* msg = error_string(err);
-        ESP_LOGE(TAG, "boot select error: %s", msg);
-        return false;
-    }
-
-    return true;
-}
-
-
-/**
- *
- * @brief Task loop
- */
-
-void Update::task() {
-    ESP_LOGI(TAG, "running");
-
-    /* Wait for wifi to be available */
-    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
-
-    // wait 60s before attempting first update check at boot
-    vTaskDelay(20000 / portTICK_RATE_MS);
-
-
-    while (true) {
-        ESP_LOGI(TAG, "checking for update");
-
-        if (this->update(update_url)) {
-            ESP_LOGI(TAG, "updated, restarting");
-            esp_restart();
-        }
-
-        vTaskDelay(90000 / portTICK_RATE_MS);
-    }
 }
 
 #endif
