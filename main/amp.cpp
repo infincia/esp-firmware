68f649cccfb803bcf52a600a05c24774ac78431f
Stephen Oliver <steve@infincia.com>
2019-06-03 23:39:32 -0400

    [Amp] increase stack size

--- main/amp.cpp
+++ main/amp.cpp
@@ -28,7 +28,7 @@ Amp::Amp() : current_volume(CONFIG_FIRMWARE_MINIMUM_VOLUME), use_spread_spectrum
 void Amp::start() {
     max9744_init();
 
-    xTaskCreate(&task_wrapper, "amp_task", 2048, this, (tskIDLE_PRIORITY + 10), &this->amp_task_handle);
+    xTaskCreate(&task_wrapper, "amp_task", 3072, this, (tskIDLE_PRIORITY + 10), &this->amp_task_handle);
 }
 
 Amp::~Amp() = default;
